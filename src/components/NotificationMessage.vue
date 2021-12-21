5716adaaf0ead2f89d79b902a76a1067cb1f95d5
Sashe Pavleski <sase_mkd@hotmail.com>
2021-12-21 05:24:02 +0100

    added notification alert div for successfully added booking alert

---
+++ src/components/NotificationMessage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="success-message">
+    {{ notification_message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotificationMessage",
+  props: {
+    notification_message: String,
+  },
+};
+</script>
+
+<style scoped>
+.success-message {
+  background-color: rgba(66, 185, 131, 0.7);
+  color: #fff;
+  font-size: 2rem;
+  border-radius: 5px;
+  padding: 10px;
+}
+</style>
